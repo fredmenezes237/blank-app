84c1b35561fe9004ce701d6be35ca262eade55d5
fredmenezes237 <119818393+fredmenezes237@users.noreply.github.com>
2025-06-10 07:58:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Storytelling Creator")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    st.text_input
+    st.selectbox
 )
